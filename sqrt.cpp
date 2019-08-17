1a8d5cb52fdb4f756a3f12915b62d922fd040baa
Tarun M Krishna <43349589+taruntmk2712@users.noreply.github.com>
2019-08-17 22:54:33 +0530

    Update sqrt.cpp

--- sqrt.cpp
+++ sqrt.cpp
@@ -4,37 +4,48 @@
 using namespace std;
 int main()
 {
+	here:
 	int n;
 	float first=0.0,last,mid,mid_1;
+	char c;
 	clock_t  start,end;
 	cout<<"Enter the number:";
 	cin>>n;
 	last = n;
 	start = clock();
-	while(last>=first)
+	while(true)
 	{
-		mid = (first+last)/2;
-		if((mid*mid) == n)
+		while(last>=first)
 		{
-			cout<<"Square root:"<<mid<<endl;
-			break;
-		}
-		else if((mid*mid) > n)
-		{
-			last = mid+1;
-		}
-		else if((mid*mid)<n)
-		{
-			mid_1 = mid;
-			while((mid_1*mid_1)<n)
+			mid = (first+last)/2;
+			if((mid*mid) == n)
 			{
-				mid_1++;
-				}
-				first = floor(mid_1);
-			cout<<"Floored sqrt:"<<first<<endl;	
-			break;
+				cout<<"Square root:"<<mid<<endl;
+				break;
+			}
+			else if((mid*mid) > n)
+			{
+				last = mid+1;
+			}
+			else if((mid*mid)<n)
+			{
+				mid_1 = mid;
+				while((mid_1*mid_1)<n)
+				{
+					mid_1++;
+					}
+					first = floor(mid_1);
+				cout<<"Floored sqrt:"<<first<<endl;	
+				break;
+			}
+
 		}
-		
+		cout<<"Do you want to continue?(y/n):"
+			cin>>c;
+		if(c=='y')
+			goto here;
+		else if(c=='n')
+			break;
 	}
 	end = clock();
  	double t = double(end-start)/CLOCKS_PER_SEC;
