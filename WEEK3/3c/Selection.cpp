b17cf1704ae3429d0bca161514058351c3043ce5
Tarun M Krishna <43349589+taruntmk2712@users.noreply.github.com>
2019-08-31 10:52:17 +0530

    Update Selection.cpp

--- WEEK3/3c/Selection.cpp
+++ WEEK3/3c/Selection.cpp
@@ -1,1 +1,36 @@
-
+#include <iostream>
+#include<ctime>
+using namespace std;
+int main()
+{
+	int a[10],n,k,mid1,count=0;
+	clock_t start,end;
+	cout<<"Enter no. of elements:";
+	cin>>n;
+	cout<<"Enter elements:";
+	for(int i =0;i<n;i++)
+	cin>>a[i];
+	start = clock();
+	for(int i=0;i<n;i++)
+	{
+		mid1=i;
+		for(int j = i+1;j<n;j++)
+			{
+				if(a[mid1]>a[j])
+				{
+					mid1 = j;
+				}
+			}
+			int temp= a[mid1];
+			a[mid1]=a[i];
+			a[i]=temp;
+			count++;
+			
+	}
+	end=clock();
+	cout<<"Count:"<<count<<endl;
+	float exe= float(end-start)/CLOCKS_PER_SEC;
+	cout<<exe;
+	
+	return 0;
+}
