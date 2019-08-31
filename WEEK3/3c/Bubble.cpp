746e58c8b6caeac41d2c7983d0782aec2e7393c0
Tarun M Krishna <43349589+taruntmk2712@users.noreply.github.com>
2019-08-31 10:55:36 +0530

    Update Bubble.cpp

--- WEEK3/3c/Bubble.cpp
+++ WEEK3/3c/Bubble.cpp
@@ -1,1 +1,33 @@
-
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int a[10],n,k,i,j,count=0;
+	clock_t start,end;
+	cout<<"Enter no. of elements:";
+	cin>>n;
+	cout<<"Enter elements:";
+	for(int i =0;i<n;i++)
+	cin>>a[i];
+	start = clock();
+	for(i =0 ; i<n;i++)
+	{
+		for(j=0;j<n-i-1;j++)
+			{
+				if(a[j]>a[j+1])
+				{
+					int temp = a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+					count++;
+				}
+			}
+	}
+	end=clock();
+	cout<<"Count:"<<count<<endl;
+	float exe= float(end-start)/CLOCKS_PER_SEC;
+	cout<<exe;
+	
+	return 0;
+}
