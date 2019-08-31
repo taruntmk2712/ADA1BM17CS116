c47cb676a26ab6cf36c48e0c27e5ae643e42799b
Tarun M Krishna <43349589+taruntmk2712@users.noreply.github.com>
2019-08-31 11:06:02 +0530

    Update Selection.cpp

--- WEEK3/3c/Selection.cpp
+++ WEEK3/3c/Selection.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-	int a[10],n,k,mid1,count=0;
+	int a[10],n,k,mid1,count=0,count1=0;
 	clock_t start,end;
 	cout<<"Enter no. of elements:";
 	cin>>n;
@@ -19,6 +19,7 @@ int main()
 				if(a[mid1]>a[j])
 				{
 					mid1 = j;
+					count1++;
 				}
 			}
 			int temp= a[mid1];
@@ -28,7 +29,7 @@ int main()
 			
 	}
 	end=clock();
-	cout<<"Count:"<<count<<endl;
+	cout<<"Count:"<<count*count1<<endl;
 	float exe= float(end-start)/CLOCKS_PER_SEC;
 	cout<<exe;
 	
