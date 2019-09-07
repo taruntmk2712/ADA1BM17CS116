45498f7fdb94d6f2c19b52862bb6128852e0db0a
Tarun M Krishna <43349589+taruntmk2712@users.noreply.github.com>
2019-09-07 10:23:16 +0530

    Update 4a.cpp

--- WEEK4/4a.cpp
+++ WEEK4/4a.cpp
@@ -4,7 +4,6 @@ int G[10][10],visited[10],n;
 void DFS(int i)
 {
 int j;
-//cout<<endl<<i;
 visited[i]=1;
 for(j=0;j<n;j++)
 {
@@ -19,7 +18,7 @@ int i,j;
 clock_t start,end;
 cout<<"Enter number of vertices:";
 cin>>n;
-cout<<"\nEnter adjecency matrix of the graph:";
+cout<<"\nEnter adjecency matrix of the graph:\n";
 for(i=0;i<n;i++)
 for(j=0;j<n;j++)
 cin>>G[i][j];
