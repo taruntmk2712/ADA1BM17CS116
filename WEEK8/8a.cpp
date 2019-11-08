42c4026d7858cbb49dc4bd9fa9ab66173c67d51e
Tarun M Krishna <43349589+taruntmk2712@users.noreply.github.com>
2019-11-08 12:01:10 +0530

    Create 8a.cpp

---
+++ WEEK8/8a.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n;
+    bool flag=true;
+    cout<<"Enter the size of the array: "<<endl;
+    cin>>n;
+    int a[n+1];
+    cout<<"Enter the array elements : "<<endl;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=1;i<=floor(n/2);i++)
+    {
+        if(2*i+1<=n)
+        {
+            if(a[i]<=max(a[2*i],a[2*i+1]))
+            {
+                flag=false;
+                break;
+            }
+        }
+        else
+        {
+            if(a[i]<=a[2*i])
+            {
+                flag = false;
+                break;
+            }
+        }
+    }
+    if(flag)
+    {
+        cout<<"Array forms a Max heap"<<endl;
+    }
+    else
+    {
+        cout<<"Array does not forms a Max heap"<<endl;
+    }#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n;
+    bool flag=true;
+    cout<<"Enter the size of the array: "<<endl;
+    cin>>n;
+    int a[n+1];
+    cout<<"Enter the array elements : "<<endl;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=1;i<=floor(n/2);i++)
+    {
+        if(2*i+1<=n)
+        {
+            if(a[i]<=max(a[2*i],a[2*i+1]))
+            {
+                flag=false;
+                break;
+            }
+        }
+        else
+        {
+            if(a[i]<=a[2*i])
+            {
+                flag = false;
+                break;
+            }
+        }
+    }
+    if(flag)
+    {
+        cout<<"Array forms a Max heap"<<endl;
+    }
+    else
+    {
+        cout<<"Array does not forms a Max heap"<<endl;
+    }
+}
+/*
+OUTPUT:
+Enter the size of the array: 10
+Enter the array elements : 70 12 30 10 8 15 20 9 5 2
+Array forms a Max heap
+*/
+}
+/*
+OUTPUT:
+Enter the size of the array: 10
+Enter the array elements : 70 12 30 10 8 15 20 9 5 2
+Array forms a Max heap
+*/
